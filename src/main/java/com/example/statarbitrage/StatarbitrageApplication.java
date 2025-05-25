aacde37a32e7a3963e967f3101c8ab3d3ce564c9
igorkhilkevich <khilkevichigor@gmail.com>
2025-05-24 23:08:04 -0400

    Initial commit

---
+++ src/main/java/com/example/statarbitrage/StatarbitrageApplication.java
@@ -0,0 +1,13 @@
+package com.example.statarbitrage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatarbitrageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatarbitrageApplication.class, args);
+	}
+
+}
