5e21f6b28d18460657dd0de9a9558ea210c2866a
igorkhilkevich <khilkevichigor@gmail.com>
2025-07-07 00:18:16 -0400

    настройки с R и фильтрацией

--- src/main/java/com/example/statarbitrage/common/utils/DbInitializer.java
+++ src/main/java/com/example/statarbitrage/common/utils/DbInitializer.java
@@ -53,3 +53,36 @@ public class DbInitializer {
     }
 
 }
+
+/*
+Норм настройки
+Settings.builder()
+                    .timeframe("15m")
+
+                    .candleLimit(300)
+                    .minWindowSize(100)
+                    .minZ(3.0)
+                    .minPValue(0.001)
+                    .minAdfValue(0.1)
+                    .minCorrelation(0.95)
+                    .minVolume(1.0)
+                    .minRSquared(0.8)
+
+                    .checkInterval(1)
+
+                    .capitalLong(10.0)
+                    .capitalShort(10.0)
+                    .leverage(10.0)
+                    .feePctPerTrade(0.05)
+
+                    .exitTake(1.0)
+                    .exitStop(0.0)
+                    .exitZMin(-3.0)
+                    .exitZMax(3.5)
+                    .exitZMaxPercent(0.0) //от 3.22 + 50% = 4.83
+                    .exitTimeHours(8)
+
+                    .usePairs(10.0)
+
+                    .build()
+ */
