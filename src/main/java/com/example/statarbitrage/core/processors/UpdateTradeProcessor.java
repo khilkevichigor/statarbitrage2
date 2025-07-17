008dcb401b0901c2a7283de81e073f766be3d829
igorkhilkevich <khilkevichigor@gmail.com>
2025-07-17 09:21:16 -0400

    Revert "refactoring UpdatePairDataRequest"
    This reverts commit 2648680320e0ca892ebce0fb92a7964f2712d2dc.

--- src/main/java/com/example/statarbitrage/core/processors/UpdateTradeProcessor.java
+++ src/main/java/com/example/statarbitrage/core/processors/UpdateTradeProcessor.java
@@ -41,13 +41,13 @@ public class UpdateTradeProcessor {
     public PairData updateTrade(UpdateTradeRequest request) {
         boolean isVirtual = tradingProviderFactory.getCurrentProvider().getProviderType().isVirtual();
         if (isVirtual) {
-            return updateVirtual(request);
+            return updateVirtualTrade(request);
         } else {
-            return updateReal(request);
+            return updateRealTrade(request);
         }
     }
 
-    private PairData updateVirtual(UpdateTradeRequest request) {
+    private PairData updateVirtualTrade(UpdateTradeRequest request) {
         PairData pairData = request.getPairData();
         boolean isCloseManually = request.isCloseManually();
         // Проверяем статус пары - если уже закрыта, не обрабатываем
@@ -70,8 +70,9 @@ public class UpdateTradeProcessor {
                 .zScoreData(zScoreData)
                 .candlesMap(candlesMap)
                 .build();
+
         pairDataService.update(updatePairDataRequest);
-        // Критично! Нужно считать профит до exitStrategy что бы сразу закрыть позиции и не ждать след захода шедуллера
+
         changesService.calculate(pairData);
 
         String exitReason = exitStrategyService.getExitReason(pairData);
@@ -92,7 +93,7 @@ public class UpdateTradeProcessor {
         return pairData;
     }
 
-    private PairData updateReal(UpdateTradeRequest request) {
+    private PairData updateRealTrade(UpdateTradeRequest request) {
         PairData pairData = request.getPairData();
         boolean isCloseManually = request.isCloseManually();
         // Перезагружаем пару из БД для получения актуального статуса
@@ -181,7 +182,7 @@ public class UpdateTradeProcessor {
                             pairData.getLongTicker(), pairData.getShortTicker());
 
                     pairData.setExitReason(exitReason);
-                    pairData.setStatus(TradeStatus.ERROR_200); //todo может ошибку сетить в отдельное поле error??? иначе на UI потеряем! или сделать на UI новую таблицу для error пар для сбора их и показа детальной инфы
+                    pairData.setStatus(TradeStatus.ERROR_200);
                 }
             }
         }
