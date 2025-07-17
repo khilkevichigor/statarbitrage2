b324764b33659bd78a729e80b24f0dce11e942ee
igorkhilkevich <khilkevichigor@gmail.com>
2025-07-17 09:21:37 -0400

    Revert "Revert "рефакторинг шедуллеров""
    This reverts commit 6a8ce05d85a21f326326714f3ec8a2e8d06e273a.

---
+++ src/main/java/com/example/statarbitrage/core/schedulers/SchedulerErrorType.java
@@ -0,0 +1,21 @@
+package com.example.statarbitrage.core.schedulers;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * Типы ошибок при выполнении шедулеров
+ */
+@Getter
+@RequiredArgsConstructor
+public enum SchedulerErrorType {
+    SCHEDULER_ALREADY_RUNNING("Шедулер уже выполняется"),
+    UPDATE_TRADES_FAILED("Ошибка при обновлении трейдов"),
+    MAINTAIN_PAIRS_FAILED("Ошибка при поддержании пар"),
+    TRADING_INTEGRATION_FAILED("Ошибка в торговой интеграции"),
+    SETTINGS_LOAD_FAILED("Ошибка загрузки настроек"),
+    INTERRUPTED_OPERATION("Операция была прервана"),
+    WAIT_TIMEOUT("Превышено время ожидания");
+
+    private final String description;
+}
