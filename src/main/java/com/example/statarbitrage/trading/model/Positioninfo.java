ee2bf8432fe7810ff7f8b95efdd0d7a2c84a2e87
igorkhilkevich <khilkevichigor@gmail.com>
2025-07-26 23:07:48 -0400

    refactor toString Positioninfo

--- src/main/java/com/example/statarbitrage/trading/model/Positioninfo.java
+++ src/main/java/com/example/statarbitrage/trading/model/Positioninfo.java
@@ -23,4 +23,26 @@ public class Positioninfo {
      * Общий PnL по закрытым позициям
      */
     private BigDecimal totalPnL;
+
+    @Override
+    public String toString() {
+        return "\nPositionInfo {\n" +
+                "  positionsClosed: " + positionsClosed + "\n" +
+                "  totalPnL: " + totalPnL + "\n" +
+                "  longPosition: " + formatPosition(longPosition) + "\n" +
+                "  shortPosition: " + formatPosition(shortPosition) + "\n" +
+                '}';
+    }
+
+    private String formatPosition(Position position) {
+        if (position == null) {
+            return "null";
+        }
+        return "\n    Position {\n" +
+                "      positionId: " + position.getPositionId() + "\n" +
+                "      symbol: " + position.getSymbol() + "\n" +
+                "      allocatedAmount: " + position.getAllocatedAmount() + "\n" +
+                "      unrealizedPnLUSDT: " + position.getUnrealizedPnLUSDT() + "\n" +
+                "    }";
+    }
 }
